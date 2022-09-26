f2d9c77ccd385a736ba7da95156fda1fc39605ee
Christian Garillo (Morning-Coffees) <garillo.cl@gmail.com>
2022-09-26 16:30:31 +0800

    Removed current code. Do it from scratch. Now using TDD and command design pattern.

--- src/main/java/toyrobot/blueprint/RobotController.java
+++
@@ -1,9 +0,0 @@
-package toyrobot.blueprint;
-
-public interface RobotController {
-	public void start();
-	public void receiveCommands();
-	void setRobotDetails();
-	void setTableDetails();
-	void parseCommand(String command, Robot robot);
-}
